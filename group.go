69453987f64883329783463e3195c81ef804daa5
Stuart <kd5pbo@gmail.com>
2014-11-06 22:23:22 +0000

    progress...

--- group.go
+++ group.go
@@ -1,6 +1,17 @@
 package sshctl
 
+import (
+	"regexp"
+)
+
 // Group represents a group of hosts, defined in the file or with
 // Set.NewGroup().
 type Group struct {
+	re   *regexp.Regexp // The regular expression that defines the group
+	name string         // The name of the group
+
+}
+
+// Add a host to the group and returns a pointer to the (changed) group
+func (g *Group) add(host Host) *Group {
 }
--- set.go
+++ set.go
@@ -6,6 +6,14 @@ import (
 
 // Set represents all the hosts read from a particular file.
 type Set struct {
+	groups map[string]*Group
+	file   string // File from which set is read
+}
+
+// New reurns a new Group as read from the specified file.  If file is "",
+// an empty group will be returned.  If file has no hosts, an empty set is
+// returned.  Any changes to the set will be written to the file.
+func New(file string) Set {
 }
 
 // Group returns a pre-defined group of hosts.
@@ -17,3 +25,5 @@ func (s Set) Group(name string) *Group {
 // retrieval with Group()
 func (s *Set) NewGroup(re regexp.Regexp) *Group {
 }
+
+// 
